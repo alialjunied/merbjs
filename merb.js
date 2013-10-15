62d028cd5a64206d4c4a31fb17f5e45491e44a9a
Victor <victorpham93@gmail.com>
2013-10-15 13:36:03 +0800

    Clear up Merb.View

--- merb.js
+++ merb.js
@@ -146,14 +146,11 @@ var delegateEventSplitter = /^(\S+)\s*(.*)$/;
 _.extend(Merb.View.prototype, {
     initialize: function(){},
     delegateEvents: function(events){
-        console.log(this);
         for(var key in events){
             var match = key.match(delegateEventSplitter);
             var eventName = match[1], selector = match[2];
-            console.log(eventName + " " + selector + "\n");
             var method = events[key];
-            this[method]();
-            $(selector).(eventName, this[method]);
+            $(selector).bind(eventName, this[method]);
         }
     },
     print: function(){
@@ -165,40 +162,5 @@ _.extend(Merb.View.prototype, {
     }
 });
 
-var extend = function(protoProps, staticProps) {
-    var parent = this;
-    var child;
-
-    // The constructor function for the new subclass is either defined by you
-    // (the "constructor" property in your `extend` definition), or defaulted
-    // by us to simply call the parent's constructor.
-    if (protoProps && _.has(protoProps, 'constructor')) {
-      child = protoProps.constructor;
-    } else {
-      child = function(){ return parent.apply(this, arguments); };
-    }
-
-    // Add static properties to the constructor function, if supplied.
-    _.extend(child, parent, staticProps);
-
-    // Set the prototype chain to inherit from `parent`, without calling
-    // `parent`'s constructor function.
-    var Surrogate = function(){ this.constructor = child; };
-    Surrogate.prototype = parent.prototype;
-    child.prototype = new Surrogate;
-
-    // Add prototype properties (instance properties) to the subclass,
-    // if supplied.
-    if (protoProps) _.extend(child.prototype, protoProps);
-
-    // Set a convenience property in case the parent's prototype is needed
-    // later.
-    child.__super__ = parent.prototype;
-
-    return child;
-  };
-
-  // Set up inheritance for the model, collection, router, view and history.
-  View.extend = extend;
 
 
